bc1c21c1a605a811bced785930d18bbfec3268b1
web3saiful <web3saiful@gmail.com>
2025-12-01 22:03:20 +0600

    "update: ongoing learning & practice iterations"

---
+++ Array.sol
@@ -0,0 +1,14 @@
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Array {
+    // Several ways to initialize an array
+    uint256[] public arr;
+    uint256[] public arr2 = [1, 2, 3];
+    // Fixed sized array, all elements initialize to 0
+    uint256[10] public myFixedSizeArr;
+
+
+}
