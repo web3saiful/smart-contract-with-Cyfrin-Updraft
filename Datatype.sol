6cdb37ab3864a07715c32a9ee1c6c98f11944491
web3saiful <web3saiful@gmail.com>
2025-12-01 21:16:42 +0600

    "update: ongoing learning & practice iterations"

---
+++ Datatype.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+​
+contract SimpleStorage {
+    // Basic types
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 88;
+    string favoriteNumberInText = "eighty-eight";
+    int256 favoriteInt = -88;
+    address myAddress = 0xaB1B7206AA6840C795aB7A6AE8b15417b7E63a8d;
+    bytes32 favoriteBytes32 = "cat";
+}
