d58900972a6fbb1d11a0f6e0a110e7d687d6afad
web3saiful <web3saiful@gmail.com>
2025-12-09 21:13:35 +0600

    "update: ongoing learning & practice iterations"

--- ContinueBreak.sol
+++ ContinueBreak.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.10;
 contract loops{
     function loop()public pure returns(uint){
         uint count=0;
-
-        uint j=0;
-        while(j<0){
-            count=count+5;
-            j=j+2;
+      
+      for(uint i=0;i<10;i=i+2){
+          
+        if(i==8){
+            continue;
         }
-        //
+        count=count+5;
+      }
+        return count;
     }
 }
