a2bdb191641383c8c124eebbfba033e01fa6ce1a
web3saiful <web3saiful@gmail.com>
2025-12-10 14:56:30 +0600

    "update: ongoing learning & practice iterations"

---
+++ SendingEther.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Receive{
+    receive()external payable {}
+    fallback()external payable {}
+
+    // check contract balance
+
+    function getBalance()public view returns(uint256){
+        return address(this).balance;
+    }
+}
+
+
+
+
+// contract sendingEther{
+
+// function sendingViaTransfer(address payable _to)public payable {
+//     _to.transfer(msg.value);
+//   }
+
+//   function sendViaSend(address payable _to)public payable {
+//     bool sent,""=_to.send(msg.value);
+//   }
+
+// }
