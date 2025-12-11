b5ad04fbf3709d87e52553911c8b85b9fe434e37
web3saiful <web3saiful@gmail.com>
2025-12-11 18:49:33 +0600

    "update: ongoing learning & practice iterations"

--- callingParentCon.sol
+++ callingParentCon.sol
@@ -48,5 +48,19 @@ contract C is A {
     }
 }
 
+contract D is B, C {
+    // Try:
+    // - Call D.foo and check the transaction logs.
+    //   Although D inherits A, B and C, it only called C and then A.
+    // - Call D.bar and check the transaction logs
+    //   D called C, then B, and finally A.
+    //   Although super was called twice (by B and C) it only called A once.
 
+    function foo() public override(B, C) {
+        super.foo();
+    }
+
+    function bar() public override(B, C) {
+        super.bar();
+    }
 }
