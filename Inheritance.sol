22985d30ffc24cd95edde24452549e96a7753f51
web3saiful <web3saiful@gmail.com>
2025-12-11 11:28:49 +0600

    "update: ongoing learning & practice iterations"

--- Inheritance.sol
+++ Inheritance.sol
@@ -19,3 +19,57 @@ contract c is a,b{
         return 'C';
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Graph of inheritance
+    A
+   / \
+  B   C
+ / \ /
+F  D,E
+
+*/
+
+contract A {
+    function foo() public pure virtual returns (string memory) {
+        return "A";
+    }
+}
+
+// Contracts inherit other contracts by using the keyword 'is'.
+contract B is A {
+    // Override A.foo()
+    function foo() public pure virtual override returns (string memory) {
+        return "B";
+    }
+}
+
+contract C is A {
+    // Override A.foo()
+    function foo() public pure virtual override returns (string memory) {
+        return "C";
+    }
+}
+
+
