0e90c67336b9796543686f6c967a6e0332fec4a1
web3saiful <web3saiful@gmail.com>
2025-12-11 18:48:41 +0600

    "update: ongoing learning & practice iterations"

---
+++ callingParentCon.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+/* Inheritance tree
+   A
+ /  \
+B   C
+ \ /
+  D
+*/
+
+contract A {
+    // This is called an event. You can emit events from your function
+    // and they are logged into the transaction log.
+    // In our case, this will be useful for tracing function calls.
+    event Log(string message);
+
+    function foo() public virtual {
+        emit Log("A.foo called");
+    }
+
+    function bar() public virtual {
+        emit Log("A.bar called");
+    }
+}
+
+contract B is A {
+    function foo() public virtual override {
+        emit Log("B.foo called");
+        A.foo();
+    }
+
+    function bar() public virtual override {
+        emit Log("B.bar called");
+        super.bar();
+    }
+}
+
+
+}
