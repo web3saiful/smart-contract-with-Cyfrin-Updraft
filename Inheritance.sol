91532baf5c74a87f1960a55e903e32cbaa520608
web3saiful <web3saiful@gmail.com>
2025-12-11 20:40:28 +0600

    "update: ongoing learning & practice iterations"

--- Inheritance.sol
+++ Inheritance.sol
@@ -1,24 +1,24 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.10;
 
-contract a{
-    function Foo()public pure virtual returns(string memory){
-        return 'A';
-    }
-}
+// contract a{
+//     function Foo()public pure virtual returns(string memory){
+//         return 'A';
+//     }
+// }
 
-contract b{
-    function Foo()public pure virtual returns (string memory){
-        return 'B';
-    }
-}
+// contract b{
+//     function Foo()public pure virtual returns (string memory){
+//         return 'B';
+//     }
+// }
 
 
-contract c is a,b{
-    function Foo()public pure override(a,b)returns (string memory){
-        return 'C';
-    }
-}
+// contract c is a,b{
+//     function Foo()public pure override(a,b)returns (string memory){
+//         return 'C';
+//     }
+// }
 
 
 
@@ -100,3 +100,7 @@ contract F is A, B {
         return super.foo();
     }
 }
+
+
+
+
