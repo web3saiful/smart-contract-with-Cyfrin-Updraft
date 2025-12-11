b76fdc06f22bfd08a3ca1c17b4c31b6adfe1d8fb
web3saiful <web3saiful@gmail.com>
2025-12-11 11:29:04 +0600

    "update: ongoing learning & practice iterations"

--- Inheritance.sol
+++ Inheritance.sol
@@ -72,4 +72,31 @@ contract C is A {
     }
 }
 
+// Contracts can inherit from multiple parent contracts.
+// When a function is called that is defined multiple times in
+// different contracts, parent contracts are searched from
+// right to left, and in a depth-first manner.
 
+contract D is B, C {
+    // D.foo() returns "C"
+    // since C is the right most parent contract with function foo()
+    function foo() public pure override(B, C) returns (string memory) {
+        return super.foo();
+    }
+}
+
+contract E is C, B {
+    // E.foo() returns "B"
+    // since B is the right most parent contract with function foo()
+    function foo() public pure override(C, B) returns (string memory) {
+        return super.foo();
+    }
+}
+
+// Inheritance must be ordered from "most base-like" to "most derived".
+// Swapping the order of A and B will throw a compilation error.
+contract F is A, B {
+    function foo() public pure override(A, B) returns (string memory) {
+        return super.foo();
+    }
+}
