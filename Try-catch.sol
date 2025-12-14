4feff4a493c2c85f3a25d615f7b6669e8c99afd5
web3saiful <web3saiful@gmail.com>
2025-12-14 21:26:04 +0600

    "update: ongoing learning & practice iterations"

---
+++ Try-catch.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+contract Foo {
+    address public owner;
+    constructor(address _owner) {
+        require(_owner != address(0), "invalid  dddddddd address");
+        assert(_owner != 0x0000000000000000000000000000000000000001);
+        owner = _owner;
+    }
+
+    function myFunc(uint256 x) public pure returns (string memory) {
+        require(x != 0, "require failed");
+        return "my func was called";
+    }
+}
+
