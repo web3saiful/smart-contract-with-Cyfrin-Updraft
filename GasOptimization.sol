fc20d384150ea1062ed4e1c15d63941a63321a56
web3saiful <web3saiful@gmail.com>
2025-12-17 12:08:14 +0600

    "update: ongoing learning & practice iterations"

---
+++ GasOptimization.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+// gas golf
+contract GasGolf {
+    // start - 50908 gas
+    // use calldata - 49163 gas
+    // load state variables to memory - 48952 gas
+    // short circuit - 48634 gas
+    // loop increments - 48244 gas
+    // cache array length - 48209 gas
+    // load array elements to memory - 48047 gas
+    // uncheck i overflow/underflow - 47309 gas
+
+
+
+}
