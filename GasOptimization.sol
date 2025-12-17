c91da464d1f2b88f782ad09d76ed2caf1e862560
web3saiful <web3saiful@gmail.com>
2025-12-17 12:08:36 +0600

    "update: ongoing learning & practice iterations"

--- GasOptimization.sol
+++ GasOptimization.sol
@@ -25,4 +25,22 @@ contract GasGolf {
     //     }
     // }
 
+    // gas optimized
+    // [1, 2, 3, 4, 5, 100]
+    function sumIfEvenAndLessThan99(uint256[] calldata nums) external {
+        uint256 _total = total;
+        uint256 len = nums.length;
+
+        for (uint256 i = 0; i < len;) {
+            uint256 num = nums[i];
+            if (num % 2 == 0 && num < 99) {
+                _total += num;
+            }
+            unchecked {
+                ++i;
+            }
+        }
+
+        total = _total;
+    }
 }
