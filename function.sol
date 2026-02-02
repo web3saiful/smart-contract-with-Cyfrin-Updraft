642c41d8770ded36e7828376603bec0313ac200f
Saiful Islam <web3saiful@gmail.com>
2026-02-02 21:39:40 +0600

    "update: ongoing learning & practice iterations"

--- function.sol
+++ function.sol
@@ -19,3 +19,22 @@ function something() public {
 }
 }
 
+
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber; // a function will update this variable
+
+    // the function will be written here
+
+
+    function store(uint256 _favoriteNumber) public {
+    favoriteNumber = _favoriteNumber;
+    uint256 testVar = 5;
+}
+​
+function something() public {
+   testVar = 6; // will raise a compilation error
+   favoriteNumber = 7; // this can be accessed because it's in the main contract scope
+}
+}
